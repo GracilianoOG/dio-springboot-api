6837eff101369be74602b3975f29dc9742675ac8
Gabriel <gabriel.gracilianomb@gmail.com>
2025-08-27 10:59:56 -0300

    Add project files

---
+++ src/main/java/com/gracilianoog/dio_springboot_api/DioSpringbootApiApplication.java
@@ -0,0 +1,13 @@
+package com.gracilianoog.dio_springboot_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioSpringbootApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DioSpringbootApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gracilianoog/dio_springboot_api/DioSpringbootApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gracilianoog.dio_springboot_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioSpringbootApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
