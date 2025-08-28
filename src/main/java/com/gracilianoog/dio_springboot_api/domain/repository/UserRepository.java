95db1b530626968d837cb5252bd894ecf4e34fcd
Gabriel <gabriel.gracilianomb@gmail.com>
2025-08-28 10:34:40 -0300

    Add Repository type

--- src/main/java/com/gracilianoog/dio_springboot_api/domain/repository/UserRepository.java
+++ src/main/java/com/gracilianoog/dio_springboot_api/domain/repository/UserRepository.java
@@ -2,7 +2,9 @@ package com.gracilianoog.dio_springboot_api.domain.repository;
 
 import com.gracilianoog.dio_springboot_api.domain.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
 }
