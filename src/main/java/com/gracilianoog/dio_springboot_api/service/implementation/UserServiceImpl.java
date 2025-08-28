4e9365d3bfe86b60f94ceb986948597a26742aef
Gabriel <gabriel.gracilianomb@gmail.com>
2025-08-28 10:51:37 -0300

    Create UserService implementation

---
+++ src/main/java/com/gracilianoog/dio_springboot_api/service/implementation/UserServiceImpl.java
@@ -0,0 +1,31 @@
+package com.gracilianoog.dio_springboot_api.service.implementation;
+
+import com.gracilianoog.dio_springboot_api.domain.model.User;
+import com.gracilianoog.dio_springboot_api.domain.repository.UserRepository;
+import com.gracilianoog.dio_springboot_api.service.UserService;
+import org.springframework.stereotype.Service;
+
+import java.util.NoSuchElementException;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public User findById(Long id) {
+        return userRepository.findById(id).orElseThrow(NoSuchElementException::new);
+    }
+
+    @Override
+    public User create(User userToCreate) {
+        if (userRepository.existsByAccountNumber(userToCreate.getAccount().getNumber())) {
+            throw new IllegalArgumentException("This Account number already exists.");
+        }
+        return userRepository.save(userToCreate);
+    }
+}
